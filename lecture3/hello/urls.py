4ea60e31f8ecf4291ac3aae4eb31a62e6a5b19a2
Christian Villalba <52127105+ChristianVillalba@users.noreply.github.com>
2021-03-17 11:22:04 +0000

    all the HttpResponse Exercises completed

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+# . the current directory
 
 urlpatterns = [
-    path("",views.index, name="index")
+    path("",views.index, name="index"),
+    # "" empty string, no additional args
+    path("<str:name>", views.greet, name="greet"),
+    path("cristian",views.cristian, name="cristian"),
+    path("cthulhu",views.cthulhu, name="cthulhu"),
+    #url pattern using placeholders
+    path("<str:name>",views.greet, name="greet"),
+
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
+
+def cristian(request):
+    return HttpResponse("This is working, Cristian")
+
+def cthulhu(request):
+    return HttpResponse("Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn")
+
+def greet(request, name): #takes an additional arg: name
+    return HttpResponse(f"Hello,{name.capitalize()}")
