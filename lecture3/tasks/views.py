5fde6094f9fd38abc7bacd74c8677f3c845fa0b9
Christian Villalba UK <christianvillalba.uk@gmail.com>
2021-05-10 15:47:57 +0100

    update addtask

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="priority", min_value=1, max_value=10, )
+    priority = forms.IntegerField(label="priority", min_value=1, max_value=5, )
 
 # Create your views here.
-tasks = ["plan","code","debug"]
+tasks = []
+
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
 
 def addTask (request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/addtask.html",{
+                "form": form
+            })
     return render(request, "tasks/addtask.html", {
         "form": NewTaskForm()
     })
