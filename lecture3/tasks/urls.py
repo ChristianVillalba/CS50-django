14c4344e80190718de7f9ec7a6ad82d2d1c2e211
Christian Villalba <52127105+ChristianVillalba@users.noreply.github.com>
2021-05-10 13:07:32 +0100

    forms

--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks"
 urlpatterns = [
     # "" empty string, no additional args
     path("",views.index, name="index"),
+    path("addtask", views.addTask, name="addtask")
 ]
--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django import forms
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="priority", min_value=1, max_value=10, )
 
 # Create your views here.
 tasks = ["plan","code","debug"]
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
+    })
+
+def addTask (request):
+    return render(request, "tasks/addtask.html", {
+        "form": NewTaskForm()
     })
